1eeb8fa7526857626448b5bb5a6ce24f6ede5462
kaede <dtdrose919@docomo.ne.jp>
2020-11-17 14:02:00 +0900

    吹き出し透過

--- app/src/main/java/jp/wings/nikkeibp/kanae28/scheduleActivity.kt
+++ app/src/main/java/jp/wings/nikkeibp/kanae28/scheduleActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import java.util.*
 
 class scheduleActivity : AppCompatActivity() {
@@ -22,6 +23,21 @@ class scheduleActivity : AppCompatActivity() {
         //時間の取得 24時間表示
         val time = calendar.get(Calendar.HOUR_OF_DAY)
 
+        //　時間に合わせてメッセージの内容を変更
+        var greetText : TextView = findViewById(R.id.message)
+
+        if( 4 <= time && time < 9 ){
+            greetText.text = "おはようございます、お嬢様。\n本日もよい一日をお過ごしくださいませ。"
+        }else if( 9 <= time && time < 15 ){
+            greetText.text = "今日もお仕事頑張ってくださいね。\n応援しております。"
+        }else if( 15 <= time && time < 18 ){
+            greetText.text = "そろそろお疲れではないですか。\n無理しないでくださいね。"
+        }else if( 18 <= time && time < 21 ){
+            greetText.text = "お帰りなさいませ、お嬢様。\nお仕事、お疲れ様でした。"
+        }else{
+            greetText.text = "お嬢様、本日もお疲れ様でした。\nおやすみなさいませ。"
+        }
+
         //時間に合わせて背景画像を変更
         val room : ImageView = findViewById<ImageView>(R.id.room)
 
@@ -32,6 +48,8 @@ class scheduleActivity : AppCompatActivity() {
         }else{
             room.setImageResource(R.drawable.room3)
         }
+
+
 
         // calenderボタンでカレンダー画面に遷移
         var Button : Button = findViewById(R.id.close)
