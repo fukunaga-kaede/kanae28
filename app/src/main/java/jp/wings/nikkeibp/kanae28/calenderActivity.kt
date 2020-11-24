d951c4bdc1d3748a29b2f44face98713908b6e79
kaede <dtdrose919@docomo.ne.jp>
2020-11-24 16:17:33 +0900

    枠完成

--- app/src/main/java/jp/wings/nikkeibp/kanae28/calenderActivity.kt
+++ app/src/main/java/jp/wings/nikkeibp/kanae28/calenderActivity.kt
@@ -40,13 +40,11 @@ class calenderActivity : AppCompatActivity() {
         //時間に合わせて背景画像を変更
         val room : ImageView = findViewById<ImageView>(R.id.room)
 
-        /*when {
+        when {
             4 <= time && time < 16 -> room.setImageResource(R.drawable.room1)
             16 <= time && time < 19 -> room.setImageResource(R.drawable.room2)
             else -> room.setImageResource(R.drawable.room3)
-        }*/
-
-        room.setImageResource(R.drawable.room1)
+        }
 
 
         // 日付のフォーマットを設定
