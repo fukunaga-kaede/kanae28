169b8a3883afceffd16c106bc4d957b55c9f0b8e
kaede <dtdrose919@docomo.ne.jp>
2020-11-24 10:42:06 +0900

    占いの☆表示

--- app/src/main/java/jp/wings/nikkeibp/kanae28/fortuneActivity.kt
+++ app/src/main/java/jp/wings/nikkeibp/kanae28/fortuneActivity.kt
@@ -1,8 +1,12 @@
 package jp.wings.nikkeibp.kanae28
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_fortune.*
 import org.json.JSONException
 import org.json.JSONObject
@@ -15,11 +19,8 @@ class fortuneActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fortune)
 
-        // アクションバーに戻るボタン（←）を追加
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        // アクションバーにタイトル「fortune」を追加
-        supportActionBar?.title = "fortune"
+        // アクションバーにタイトル「home」を追加
+        supportActionBar?.title = "home"
 
 
         val assetManager = resources.assets //アセット呼び出し
@@ -34,27 +35,88 @@ class fortuneActivity : AppCompatActivity() {
             val jsonArray = jsonObject.getJSONArray("fortune")
 
             //1～12のランダムな整数をiに代入
-            val i = Random().nextInt(12)
+            // val i = Random().nextInt(12)
+            val i = 0;
 
             val jsonData = jsonArray.getJSONObject(i)
-            textView1.text = "${jsonData.getString("content")}"
-            textView2.text = "ラッキーアイテム : ${jsonData.getString("item")}"
-            textView3.text = "ラッキーカラー : ${jsonData.getString("color")}"
+            message.text = "${jsonData.getString("content")}"
+            item.text = "ラッキーアイテム : ${jsonData.getString("item")}"
+            color.text = "ラッキーカラー : ${jsonData.getString("color")}"
 
+            val totalcount = jsonData.getString("total")
+
+            when {
+                totalcount == "1" -> totalstar.text  = "☆"
+                totalcount == "2" -> totalstar.text  = "☆☆"
+                totalcount == "3" -> totalstar.text  = "☆☆☆"
+                totalcount == "4" -> totalstar.text  = "☆☆☆☆"
+                totalcount == "5" -> totalstar.text  = "☆☆☆☆☆"
+                else -> totalstar.text  = ""
+            }
+
+            val lovecount = jsonData.getString("love")
+
+            when {
+                lovecount == "1" -> lovestar.text  = "☆"
+                lovecount == "2" -> lovestar.text  = "☆☆"
+                lovecount == "3" -> lovestar.text  = "☆☆☆"
+                lovecount == "4" -> lovestar.text  = "☆☆☆☆"
+                lovecount == "5" -> lovestar.text  = "☆☆☆☆☆"
+                else -> lovestar.text  = ""
+            }
+
+            val jobcount = jsonData.getString("job")
+
+            when {
+                jobcount == "1" -> jobstar.text  = "☆"
+                jobcount == "2" -> jobstar.text  = "☆☆"
+                jobcount == "3" -> jobstar.text  = "☆☆☆"
+                jobcount == "4" -> jobstar.text  = "☆☆☆☆"
+                jobcount == "5" -> jobstar.text  = "☆☆☆☆☆"
+                else -> totalstar.text  = ""
+            }
+
+            val moneycount = jsonData.getString("money")
+
+            when {
+                moneycount == "1" -> moneystar.text  = "☆"
+                moneycount == "2" -> moneystar.text  = "☆☆"
+                moneycount == "3" -> moneystar.text  = "☆☆☆"
+                moneycount == "4" -> moneystar.text  = "☆☆☆☆"
+                moneycount == "5" -> moneystar.text  = "☆☆☆☆☆"
+                else -> totalstar.text  = ""
+            }
 
         }catch (e: JSONException) {
             e.printStackTrace()
         }
-    }
 
-    // 戻るボタンの機能を実装
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
-            android.R.id.home -> finish()
-            else -> return super.onOptionsItemSelected(item)
+        //現在時刻の取得
+        val calendar: Calendar = Calendar.getInstance(
+            TimeZone.getTimeZone("Asia/Tokyo"),
+            Locale.JAPAN
+        );
+
+        //時間の取得 24時間表示
+        val time = calendar.get(Calendar.HOUR_OF_DAY)
+
+        //時間に合わせて背景画像を変更
+        val room : ImageView = findViewById<ImageView>(R.id.room)
+
+        if( 4 <= time && time < 16 ){
+            room.setImageResource(R.drawable.room1)
+        }else if( 16 <= time && time < 19 ){
+            room.setImageResource(R.drawable.room2)
+        }else{
+            room.setImageResource(R.drawable.room3)
         }
-        return true
+
+        // 「×」ボタンで消える
+        var Button : Button = findViewById(R.id.close)
+
+        Button.setOnClickListener {
+            val intent = Intent(this@fortuneActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
-
-
 }
